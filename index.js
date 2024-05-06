5b7294ab0267fd4be6daa184088cb297365bf670
fadi <fadishqerat54@icloud.com>
2024-05-06 21:44:48 +0300

    s
    s

--- index.js
+++ index.js
@@ -25,4 +25,8 @@ app.use(morgan("dev"));
 // route middlewares
 app.use("/api", authRoutes);
 
-app.listen(8000,() => console.log("Server running on port 8000"));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+// app.listen(8000,() => console.log("Server running on port 8000"));
